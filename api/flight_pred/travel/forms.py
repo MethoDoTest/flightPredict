548c084fbf15c293353b7dbbcbc93e6017a7aff4
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-06-25 17:46:08 +0200

    every forget fields added

--- api/flight_pred/travel/forms.py
+++ api/flight_pred/travel/forms.py
@@ -4,6 +4,17 @@ import re
 class TravelForm(forms.Form):
     departure = forms.CharField(label='depart', max_length=100, required=True)
     destination = forms.CharField(label='destination', max_length=100, required=True)
+    airline = forms.CharField(label='airline', max_length=100, required=True)
+    total_stops = forms.IntegerField(label='total_stops', required=True)
+    date = forms.IntegerField(label='date', required=True)
+    month = forms.IntegerField(label='month', required=True)
+    year = forms.IntegerField(label='year', required=True)
+    dep_hours = forms.IntegerField(label='dep_hours', required=True)
+    dep_min = forms.IntegerField(label='dep_min', required=True)
+    arrival_hours = forms.IntegerField(label='arrival_hours', required=True)
+    arrival_min = forms.IntegerField(label='arrival_min', required=True)
+    duration_hours = forms.IntegerField(label='duration_hours', required=True)
+    duration_min = forms.IntegerField(label='duration_min', required=True)
 
     def clean_departure(self):
         departure = self.cleaned_data['departure']
@@ -16,4 +27,69 @@ class TravelForm(forms.Form):
         if not re.match("^[A-Za-z\s]*$", destination):
             raise forms.ValidationError('le champ "destination" ne doit contenir que des lettres et des espaces !')
         return destination
-    
+
+    def clean_airline(self):
+        airline = self.cleaned_data['airline']
+        if not re.match("^[A-Za-z\s]*$", airline):
+            raise forms.ValidationError('le champ "airline" ne doit contenir que des lettres et des espaces !')
+        return airline
+
+    def clean_total_stops(self):
+        total_stops = self.cleaned_data['total_stops']
+        if not isinstance(total_stops, int):
+            raise forms.ValidationError('le champ "total_stops" ne doit contenir qu\'un nombre !')
+        return total_stops
+
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if not (1 <= date <= 31):
+            raise forms.ValidationError('le champ "date" doit être un nombre entre 1 et 31 !')
+        return date
+
+    def clean_month(self):
+        month = self.cleaned_data['month']
+        if not (1 <= month <= 12):
+            raise forms.ValidationError('le champ "month" doit être un nombre entre 1 et 12 !')
+        return month
+
+    def clean_year(self):
+        year = self.cleaned_data['year']
+        if not (2000 < year < 2100):
+            raise forms.ValidationError('le champ "year" doit être un nombre entre 2001 et 2099 !')
+        return year
+
+    def clean_dep_hours(self):
+        dep_hours = self.cleaned_data['dep_hours']
+        if not (0 <= dep_hours <= 24):
+            raise forms.ValidationError('le champ "dep_hours" doit être un nombre entre 0 et 24 !')
+        return dep_hours
+
+    def clean_dep_min(self):
+        dep_min = self.cleaned_data['dep_min']
+        if not (0 <= dep_min <= 60):
+            raise forms.ValidationError('le champ "dep_min" doit être un nombre entre 0 et 60 !')
+        return dep_min
+
+    def clean_arrival_hours(self):
+        arrival_hours = self.cleaned_data['arrival_hours']
+        if not (0 <= arrival_hours <= 24):
+            raise forms.ValidationError('le champ "arrival_hours" doit être un nombre entre 0 et 24 !')
+        return arrival_hours
+
+    def clean_arrival_min(self):
+        arrival_min = self.cleaned_data['arrival_min']
+        if not (0 <= arrival_min <= 60):
+            raise forms.ValidationError('le champ "arrival_min" doit être un nombre entre 0 et 60 !')
+        return arrival_min
+
+    def clean_duration_hours(self):
+        duration_hours = self.cleaned_data['duration_hours']
+        if not isinstance(duration_hours, int):
+            raise forms.ValidationError('le champ "duration_hours" ne doit contenir qu\'un nombre !')
+        return duration_hours
+
+    def clean_duration_min(self):
+        duration_min = self.cleaned_data['duration_min']
+        if not (0 <= duration_min <= 60):
+            raise forms.ValidationError('le champ "duration_min" doit être un nombre entre 0 et 60 !')
+        return duration_min
--- api/flight_pred/travel/views.py
+++ api/flight_pred/travel/views.py
@@ -2,37 +2,56 @@ from django.shortcuts import render
 from .forms import TravelForm
 import requests
 
-
 def travel_view(request):
     result = None
     errors = None
-    if request.method == "POST":
-        if "submit" in request.POST:
+    if request.method == 'POST':
+        if 'submit' in request.POST:
             form = TravelForm(request.POST)
             if form.is_valid():
-                departure = form.cleaned_data["departure"]
-                destination = form.cleaned_data["destination"]
+                departure = form.cleaned_data['departure']
+                destination = form.cleaned_data['destination']
+                airline = form.cleaned_data['airline']
+                total_stops = form.cleaned_data['total_stops']
+                date = form.cleaned_data['date']
+                month = form.cleaned_data['month']
+                year = form.cleaned_data['year']
+                dep_hours = form.cleaned_data['dep_hours']
+                dep_min = form.cleaned_data['dep_min']
+                arrival_hours = form.cleaned_data['arrival_hours']
+                arrival_min = form.cleaned_data['arrival_min']
+                duration_hours = form.cleaned_data['duration_hours']
+                duration_min = form.cleaned_data['duration_min']
 
-                api_url = (
-                    "http://127.0.0.1:8000/travel/"  # remplacer par l'url de l'api !
-                )
-                data = {"departure": departure, "destination": destination}
+                "http://127.0.0.1:8000/travel/"  # remplacer par l'url de l'api !
+                data = {
+                    'departure': departure,
+                    'destination': destination,
+                    'airline': airline,
+                    'total_stops': total_stops,
+                    'date': date,
+                    'month': month,
+                    'year': year,
+                    'dep_hours': dep_hours,
+                    'dep_min': dep_min,
+                    'arrival_hours': arrival_hours,
+                    'arrival_min': arrival_min,
+                    'duration_hours': duration_hours,
+                    'duration_min': duration_min
+                }
                 try:
                     response = requests.post(api_url, json=data)
                     response.raise_for_status()
-                    result = response.json().get(
-                        "result", "Aucun résultat trouvé"
-                    )  # à modifier !
+                    result = response.json().get('result', 'Aucun résultat trouvé')  # à modifier !
                 except requests.RequestException as e:
                     errors = f"Erreur lors de l'appel à l'API: {e}"
             else:
                 errors = form.errors
-        elif "clear" in request.POST:
+        elif 'clear' in request.POST:
             form = TravelForm()
     else:
         form = TravelForm()
-    return render(
-        request,
-        "travel/travel_form.html",
-        {"form": form, "result": result, "errors": errors},
-    )
+    return render(request, 'travel/travel_form.html', {'form': form, 'result': result, 'errors': errors})
+
+
+
