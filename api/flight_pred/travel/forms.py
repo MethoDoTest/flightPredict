7cecc2579c97a4d49b934217f5c96a4df001cba0
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-06-25 14:22:37 +0200

    clear button and simu pred text result
    Affichage d'une phrase qui sera remplacée par le résultat de la prédiction du modèle ainsi que l'ajout d'un bouton clear permettant d'effacer le résultat et le contenu des champs

--- api/flight_pred/travel/forms.py
+++ api/flight_pred/travel/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class TravelForm(forms.Form):
-    departure = forms.CharField(label='départ', max_length=100)
+    departure = forms.CharField(label='depart', max_length=100)
     destination = forms.CharField(label='destination', max_length=100)
--- api/flight_pred/travel/views.py
+++ api/flight_pred/travel/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from .forms import TravelForm
 
 def travel_view(request):
+    result = None
     if request.method == 'POST':
-        form = TravelForm(request.POST)
-        if form.is_valid():
-            departure = form.cleaned_data['departure']
-            destination = form.cleaned_data['destination']
-            # traitement des données
+        if 'submit' in request.POST:
+            form = TravelForm(request.POST)
+            if form.is_valid():
+                departure = form.cleaned_data['departure']
+                destination = form.cleaned_data['destination']
+                # traitement des données
+                result = 'price pred result'
+        elif 'clear' in request.POST:
+            form = TravelForm()
     else:
         form = TravelForm()
-    return render(request, 'travel/travel_form.html', {'form': form})
+    return render(request, 'travel/travel_form.html', {'form': form, 'result': result})
