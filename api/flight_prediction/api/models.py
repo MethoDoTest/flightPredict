1a92c6fea85986d580c79480f2cc83bf99fadf88
AdrienP83 <65600747+AdrienP83@users.noreply.github.com>
2024-06-25 11:37:49 +0200

    init de model.py  avec le dataset

--- api/flight_prediction/api/models.py
+++ api/flight_prediction/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class FlightPrice(models.Model):
+    airline = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    total_stops = models.IntegerField()
+    price = models.FloatField()
+    date = models.IntegerField()
+    month = models.IntegerField()
+    year = models.IntegerField()
+    dep_hours = models.IntegerField()
+    dep_min = models.IntegerField()
+    arrival_hours = models.IntegerField()
+    arrival_min = models.IntegerField()
+    duration_hours = models.IntegerField()
+    duration_min = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.date}/{self.month}/{self.year} | {self.airline} | {self.source} to {self.destination} | {self.price}"
+
+    class Meta:
+        verbose_name = "Flight Price"
+        verbose_name_plural = "Flight Prices"
