f47ced38a62fc593d898be5b75e832d809912f77
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-06-25 14:57:33 +0200

    error message -> red

--- api/flight_pred/travel/forms.py
+++ api/flight_pred/travel/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+import re
 
 class TravelForm(forms.Form):
-    departure = forms.CharField(label='depart', max_length=100)
-    destination = forms.CharField(label='destination', max_length=100)
+    departure = forms.CharField(label='depart', max_length=100, required=True)
+    destination = forms.CharField(label='destination', max_length=100, required=True)
+
+    def clean_departure(self):
+        departure = self.cleaned_data['departure']
+        if not re.match("^[A-Za-z\s]*$", departure):
+            raise forms.ValidationError('le champ "depart" ne doit contenir que des lettres et des espaces !')
+        return departure
+
+    def clean_destination(self):
+        destination = self.cleaned_data['destination']
+        if not re.match("^[A-Za-z\s]*$", destination):
+            raise forms.ValidationError('le champ "destination" ne doit contenir que des lettres et des espaces !')
+        return destination
+    
--- api/flight_pred/travel/views.py
+++ api/flight_pred/travel/views.py
@@ -3,16 +3,18 @@ from .forms import TravelForm
 
 def travel_view(request):
     result = None
+    errors = None
     if request.method == 'POST':
         if 'submit' in request.POST:
             form = TravelForm(request.POST)
             if form.is_valid():
                 departure = form.cleaned_data['departure']
                 destination = form.cleaned_data['destination']
-                # traitement des données
-                result = 'price pred result'
+                result = 'result' # affichage du résultat prédit 
+            else:
+                errors = form.errors
         elif 'clear' in request.POST:
             form = TravelForm()
     else:
         form = TravelForm()
-    return render(request, 'travel/travel_form.html', {'form': form, 'result': result})
+    return render(request, 'travel/travel_form.html', {'form': form, 'result': result, 'errors': errors})
