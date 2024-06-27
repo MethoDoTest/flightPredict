8808b2352697b6ae96e59cf39a1c52925170f995
AdrienP83 <65600747+AdrienP83@users.noreply.github.com>
2024-06-27 09:29:19 +0200

    error solved in views.py

--- api/flight_pred/travel/views.py
+++ api/flight_pred/travel/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from .forms import TravelForm
 import requests
 
 
+@csrf_exempt
 def travel_view(request):
     result = None
     errors = None
+    form = TravelForm()  # Assurez-vous que form est toujours défini
+
     if request.method == "POST":
         if "submit" in request.POST:
             form = TravelForm(request.POST)
@@ -14,7 +18,7 @@ def travel_view(request):
                 destination = form.cleaned_data["destination"]
 
                 api_url = (
-                    "http://127.0.0.1:8000/travel/"  # remplacer par l'url de l'api !
+                    "http://127.0.0.1:8000/travel/"  # Remplacer par l'URL de l'API !
                 )
                 data = {"departure": departure, "destination": destination}
                 try:
@@ -22,15 +26,14 @@ def travel_view(request):
                     response.raise_for_status()
                     result = response.json().get(
                         "result", "Aucun résultat trouvé"
-                    )  # à modifier !
+                    )  # À modifier selon la réponse de votre API
                 except requests.RequestException as e:
                     errors = f"Erreur lors de l'appel à l'API: {e}"
             else:
                 errors = form.errors
         elif "clear" in request.POST:
             form = TravelForm()
-    else:
-        form = TravelForm()
+
     return render(
         request,
         "travel/travel_form.html",
