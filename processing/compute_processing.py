e8acd07b9e02b221907c4049d1aa18f2fdf9efa4
Mbaye44 <bambambaye1993@gmail.com>
2024-07-08 13:34:08 +0200

    Fonction processing et compute

---
+++ processing/compute_processing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from processing_fonction import load_data, analyze_missing_values, generate_feature_report, create_new_features, preprocess_data
+
+# Chemin du fichier
+file_path = '..\\dataset\\rawData\\flight_dataset.csv'
+
+# Chargement des données
+df = load_data(file_path)
+
+# Analyse des valeurs manquantes
+analyze_missing_values(df)
+
+# Génération du rapport de caractéristiques
+generate_feature_report(df)
+
+# Création de nouvelles caractéristiques
+df = create_new_features(df)
+
+# Prétraitement des données
+X, y = preprocess_data(df)
+
+# Affichage des premières lignes pour vérifier
+print("Training Features DataFrame:")
+print(X.head())
+print("\nTarget Series:")
+print(y.head())
