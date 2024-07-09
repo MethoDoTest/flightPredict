55d34adea5201c1a458eedb3546f7975e517e2f4
AdrienP83 <65600747+AdrienP83@users.noreply.github.com>
2024-07-09 15:06:59 +0200

    accpet incoming change

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,17 +1,13 @@
 # test_metrics.py
 import pytest
 import numpy as np
-<<<<<<< HEAD:dev/metrics_capture/test_metrics.py
-from metrics import (
+from model_eval.metrics import (
     mean_squared_error,
     r2_score,
     root_mean_squared_error,
     mean_absolute_error,
 )
 
-=======
-from model_eval.metrics import mean_squared_error, r2_score, root_mean_squared_error, mean_absolute_error
->>>>>>> 746263f98426ff925a909c42804d1dc626cc1874:tests/test_metrics.py
 
 class MockFlightPriceModel:
     def predict(self, X):
